ff7bf5b740a8a978f1253b6babbd0aa4af60ae7a
otienofelo <otienofelitichia@gmail.com>
2026-02-25 11:27:10 +0300

    modified gitignore and firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,23 +1,19 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { setDoc,doc } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAC1xbZ5eq-xA67EUNw0kMIkuqSJzhiAg4",
-  authDomain: "livestock-managment-syst-12651.firebaseapp.com",
-  projectId: "livestock-managment-syst-12651",
-  storageBucket: "livestock-managment-syst-12651.firebasestorage.app",
-  messagingSenderId: "451430855454",
-  appId: "1:451430855454:web:11fdf409368fa301e94f0f"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
 export default app;
